66809215cd74c9aad294cd7f970301770e44c2a9
n-flam <nadja.flam@gmx.net>
2021-03-24 12:00:09 +0800

    const route in api.js
    1 get route

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,26 @@
-// const router expres
-// requer model workout
 //  5 routes: 1 post, 1 put, 2 get, 1 delete
+
+// const router expres
+const router = require('express').Router();
+const Workout = require('../models/workout.js');
+
+// 1 get get route
+router.get('/api/workouts', (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    },
+  ])
+    .then((workoutDBs) => {
+      res.json(workoutDBs);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// requier model workout
