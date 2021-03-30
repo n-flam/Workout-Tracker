b306df3f944e01b27f55776f97bedf722bd0e7e4
nadja <nadja.flam@gmx.net>
2021-03-30 18:33:30 +0800

    sever.js and read mean

--- server.js
+++ server.js
@@ -12,28 +12,27 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_DSN, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(
+  process.env.MONGODB_DSN || 'mongodb://localhost/workout-tracker-nflam',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+  );
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-    console.log("yes we are connected");
+db.once("open", function () {
+  console.log("yes we are connected");
 });
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker-nflam',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
 
 // routes
 app.use(require("./routes/api-routes"));
 app.use(require("./routes/html-routes"));
 
 app.listen(3000, () => {
-    console.log("App running on port 3000!");
-  }); 
+  console.log("App running on port 30`App running on port ${PORT}! Visit http://localhost:${PORT}/ in your browser");
+});
